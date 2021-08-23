a55192bed707e1db9aa2ef96e2ed79efdcae96c6
Christian Muñoz <chgeovany.15@gmail.com>
2021-08-23 16:19:18 -0500

    Funciones

---
+++ function.c
@@ -0,0 +1,11 @@
+
+/*return-type function-name(parameter declarations, if any)
+{
+    declararions
+    statements
+}*/
+
+int powerOfIntegers(int base, int n)
+{
+    
+}
---
+++ functiontyperPerReturnType.c
@@ -0,0 +1,35 @@
+//Funciones sin argumentos y sin valor de retorno
+
+void functionName();
+
+functionName();
+
+void functionName()
+{
+    //logica
+}
+
+//Funciones con argumentos pero sin valores a retornar
+
+//returntype --> void functionName(int);
+void functionName(float);
+void functionName(long);
+
+
+functionName(float);
+
+void functionName(float)
+{
+    //logica
+}
+
+//Funciones sin argumentos pero que si tienen valores a retornar
+
+float functionName();
+
+//Funciones con argumentos con return value
+
+float functionName(int, float);
+
+
+
